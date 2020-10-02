5603758b736169f616feffeed99b8a8937ccb51c
simranverma7 <72251268+simranverma7@users.noreply.github.com>
2020-10-02 17:00:29 +0530

    Create squareroot.cpp
    This program takes a input and prints its square root b

---
+++ squareroot.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream.h>
+#include<math.h>
+using namespace std;
+
+int main() {
+	
+	int t;
+  float a,b;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+	    cin>>a;
+	    b=(float)(sqrt(a));
+	    cout<<b<<endl;
+	}
+	return 0;
