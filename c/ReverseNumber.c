2dfc3255c8d78426451432f0e9438a4dd7a87079
rebbasaikiran <[rebba.saikiran@gdn-commerce.com]>
2020-10-02 20:00:03 +0530

    condition added for overflowd numbers

--- c/ReverseNumber.c
+++ c/ReverseNumber.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
 int reverseNum (int num) {
-  int rev = 0;
+  int rev = 0, prev_rev = 0;
   int negFlag = 1; // flag for negative or positive num
   if (num < 0) {
       negFlag = -1;
       num = -num;
   }
-  while (num > 0) {
-      rev = rev*10 + num%10;
+  while (num != 0) {
+      int curr_digit = num%10;
+      rev = rev*10 + curr_digit;
+      // checking whether the num is overflowed or not
+      if ((rev - curr_digit)/10 != prev_rev) {
+          printf("WARNING OVERFLOWED!!!");
+          return 0;
+      }
+      prev_rev = rev;
       num = num/10;
   }
   return (negFlag < 1) ? -rev : rev;
