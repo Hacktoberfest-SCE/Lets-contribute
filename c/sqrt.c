614792a16eea73b853bb39dd72b087184237be3f
ragreenburg <ragreenburg@gmail.com>
2020-10-02 10:03:25 -0700

    sqrt of user input in C

---
+++ c/sqrt.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int testInteger;
+    printf("Enter an integer: ");
+    scanf("%d", &testInteger); 
+    printf("Square root = %f", sqrt(testInteger));
+    return 0;
+}
