c32cccdbcb345975420c1d0ea62d0f91186f6d18
Anjali Jha <72214468+Anjali-Jha22@users.noreply.github.com>
2020-10-02 17:12:21 +0530

    Create ASCII.cpp
    This file is created to return the ASCII value

---
+++ c++/ASCII.cpp
@@ -0,0 +1,12 @@
+//Program to input a character and return the ASCII value of a character as output using cpp 
+#include <iostream>
+//header file for input output operations
+using namespace std;
+
+int main()
+{
+    char d;
+    cin>>d;
+    cout<<int(d);
+
+}
