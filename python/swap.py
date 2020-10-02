8e3d1c1257ac4ab2464d3f8661190caca912aa41
nikithabk <72269847+nikithabk@users.noreply.github.com>
2020-10-03 01:07:39 +0530

    Create swap.py

---
+++ python/swap.py
@@ -0,0 +1,8 @@
+# python code to swap two numbers
+# without using 3rd variable
+x = int(input("Enter first number:"))
+y = int(input("enter second number:"))
+print("before swapping x:",x,"y:",y)
+# code to swap 'x' and 'y'
+x, y = y, x
+print("after swapping x:",x,"y:",y)
