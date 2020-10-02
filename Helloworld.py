cb06f5dddb73a706d08b5556c7cac7447364199a
Kausar-De <71579656+Kausar-De@users.noreply.github.com>
2020-10-02 12:05:09 +0530

    Update Helloworld.py

--- Helloworld.py
+++ Helloworld.py
@@ -1,3 +1,3 @@
-#Program to print Hello World in Python
-#One of my first PR's
-print("Hello World!")
+#Tried to add a twist to the typical "Hello World!" program :P
+x = ['H','e','l','l','o',' ','W','o','r','l','d','!']
+print(*x, sep = '')
