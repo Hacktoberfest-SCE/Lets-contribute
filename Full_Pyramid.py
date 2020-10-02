b760a274810d0357ea2ecde51af985d4d057ec1b
Bhupi100 <71540345+Bhupi100@users.noreply.github.com>
2020-10-02 13:49:43 +0530

    Create Full_Pyramid.py

---
+++ Full_Pyramid.py
@@ -0,0 +1,11 @@
+#Take int N as input which denotes n rows and prits Full Pyramid
+size = int(input())
+m = (2 * size) - 2
+for i in range(0, size):
+    for j in range(0, m):
+        print(end=" ")
+    m = m - 1  # decrementing m after each loop
+    for j in range(0, i + 1):
+        # printing full Triangle pyramid using stars
+        print("* ", end=' ')
+    print(" ")
