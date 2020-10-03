e212faba149ad5c1fbab35aa7efc9e906fb2754d
shashankmagaji <72260187+shashankmagaji@users.noreply.github.com>
2020-10-03 17:17:42 +0530

    Create BMI.cpp
    this file is created to measure the BMI

---
+++ c++/BMI.cpp
@@ -0,0 +1,21 @@
+//program to measure the BMI
+#include<iostream>
+#include<math.h>
+int main()
+{
+    float bmi,w,h;
+    cout<<"Please Enter your weight in Pounds : ";
+    cin>>w;
+    cout<<"Please Enter your Height in Inches : ";
+    cin>>h;
+    bmi=(w*703)/(h*h);
+    cout<<"Your BMI is : "<<bmi<<endl;
+ 
+    if(bmi>25)
+    cout<<"Overweight";
+    else if(bmi<25 && bmi>18.5)
+            cout<<"Optimal";
+            else
+            cout<<"Underweight";
+    return 0;
+}
