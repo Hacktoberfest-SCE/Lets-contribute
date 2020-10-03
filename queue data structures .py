7feca024fbd7e58747e1bb6b8f4ebc701fe3089f
KHUSHI068 <70849038+KHUSHI068@users.noreply.github.com>
2020-10-03 23:08:25 +0530

    Create queue data structures .py
    printing a queue

---
+++ queue data structures .py
@@ -0,0 +1,19 @@
+CODE OF QUEUE DATA STRUCTURES IN PYTHON
+#initialising a queue
+queue=[]
+#adding elements to the queue 
+queue.append('a')
+queue.append('b')
+queue.append('c')
+print("Initial queue")
+print(queue)
+#removing elements from the queue
+print("\nElements dequeued from queue")
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+print("\nQueue after removing elements")
+print(queue)
+#Uncommenting print(queue.pop(0))
+#will raise and IndexError
+#as the queue is now empty
