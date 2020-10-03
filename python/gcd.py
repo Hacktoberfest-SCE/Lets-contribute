14581ff6276ed4f0474a4c75f4b665ab2953f0ee
Anjali Jha <72214468+Anjali-Jha22@users.noreply.github.com>
2020-10-03 10:37:55 +0530

    Create gcd.py
    Pull request for gcd of numbers

---
+++ python/gcd.py
@@ -0,0 +1,8 @@
+# Python code to demonstrate gcd() using python 3
+import math
+#to avail gcd()
+a=int(input())
+b=int(input())
+
+print ("The gcd is : ",end="") 
+print (math.gcd(a,b)) #importing math to avail gcd()
