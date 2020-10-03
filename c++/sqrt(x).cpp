cb1ae85a581b0177bff1b3d91c71711b120dc099
shashankmagaji <72260187+shashankmagaji@users.noreply.github.com>
2020-10-03 17:04:49 +0530

    Create sqrt(x).cpp
    this file is created to use the sqrt(x)

---
+++ c++/sqrt(x).cpp
@@ -0,0 +1,11 @@
+//program to check the sqrt(x)
+#include <iostream>
+#include <math.h>
+int main()
+{
+	double x = 10.25, result;
+	result = sqrt(x);
+	cout << "Square root of " << x << " is " << result << endl;
+
+	return 0;
+}
