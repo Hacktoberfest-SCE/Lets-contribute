b4f86fdd6af1d78e089a8dbdcb9dc21357d83361
Aanchal315 <72307884+Aanchal315@users.noreply.github.com>
2020-10-03 23:24:56 +0530

    Create sqrt.java
    squarroot of input x

---
+++ java/sqrt.java
@@ -0,0 +1,22 @@
+import java.lang.Math; 
+  
+class Gfg {
+    public static void main(String args[]) 
+    { 
+        double x = 30; 
+  
+        System.out.println(Math.sqrt(x)); 
+  
+        x = 45; 
+  
+        System.out.println(Math.sqrt(x)); 
+  
+        x = 60; 
+  
+        System.out.println(Math.sqrt(x)); 
+  
+        x = 90; 
+  
+        System.out.println(Math.sqrt(x)); 
+    } 
+} 
