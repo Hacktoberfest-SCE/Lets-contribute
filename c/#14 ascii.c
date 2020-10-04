2a9ba6017652930306890e57daefcc68a6ce89a4
Akanksha11909 <72248968+Akanksha11909@users.noreply.github.com>
2020-10-04 14:28:24 +0530

    Add files via upload

---
+++ c/#14 ascii.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	char character;
+	printf("Enter the character\n");
+	scanf("%c", &character);
+	printf("ASCII value of %c is %d", character, character);
+	return 0;
+	
+}
